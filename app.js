cc7d4da6776e611d16c626d09e389e0a9d23e295
Amartya Chowdhury <132732400+widewipe@users.noreply.github.com>
2023-06-15 10:34:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,14 +55,6 @@ app.get('/posts/:pname', function(req, res){
 
 
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server started . . .");
 });
